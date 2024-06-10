a028107045cce2e7d89d6acce6072e38c123b808
Antonio Berben <antonio.berben@solo.io>
2024-06-10 21:54:11 +0200

    .

--- jaeger/README.md
+++
@@ -1,6 +0,0 @@
-# Jaeger
-
-```
-helm upgrade --install jaeger jaegertracing/jaeger -n jaeger --create-namespace \
--f jaeger-values.yaml --version 0.73.1
-```
--- tcp-services/mysql/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS mydatabase;
-USE mydatabase;
-CREATE TABLE IF NOT EXISTS demo (ping VARCHAR(255));
-INSERT INTO demo (ping) VALUES (CONCAT('pong_', @@hostname));
